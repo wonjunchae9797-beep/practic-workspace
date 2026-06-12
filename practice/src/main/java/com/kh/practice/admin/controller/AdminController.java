5c9327388cbfbafe40f00941e986ca1079d35c35
kkimmun <kimsungyeom007@naver.com>
2026-06-12 17:56:06 +0900

    ㅇㅇ

--- practice/src/main/java/com/kh/practice/admin/controller/AdminController.java
+++ practice/src/main/java/com/kh/practice/admin/controller/AdminController.java
@@ -1,9 +1,12 @@
 package com.kh.practice.admin.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.practice.admin.model.service.AdminService;
+import com.kh.practice.api.model.vo.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,4 +19,6 @@ public class AdminController {
 	
 	private final AdminService adminService;
 	
+	@PatchMapping
+	public ResponseEntity<ApiResponse<Void>> 
 }
